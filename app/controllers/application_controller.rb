091a779aa89076249ddc7a3d898b35838e9f2070
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-06-25 12:27:46 +0000

    [add] admin_login_seesion_setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
 
-  def after_sign_in_path_for(resource)
-    case resource
-
-    # admin
-    when Admin
-      admin_root_path
-    end
-  end
-
-  def after_sign_out_path_for(resource)
-    case resource
-
-    # admin(↓error発生)
-    when Admin
-      new_admin_session_path
-    end
-  end
-
 end
