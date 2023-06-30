8fb59199269f7f0b18f6601c82adf25fdffb031b
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-06-30 13:36:17 +0000

    [add] confirm_process

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -25,15 +26,32 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-    # Public_sign_in
-    def after_sign_in_path_for(resource)
-      root_path
+  # Public_sign_in
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # Public_sign_out
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  def customer_state
+
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+
+    # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+
+    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致しているか判別
+    # 【処理内容3】 【処理内容2】・is_deleted = true であった場合、ログイン不可の処理とする。（サインアップ画面に戻す）
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+      redirect_to new_customer_registration_path
     end
 
-    # Public_sign_out
-    def after_sign_out_path_for(resource)
-      root_path
-    end
-
+  end
 
 end
