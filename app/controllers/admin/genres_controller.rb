42fd5985040a6a581c7bb0bdb2eb5b8a5186e89d
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-02 08:41:40 +0000

    [add] genre_edit_process

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,14 +7,18 @@ class Admin::GenresController < ApplicationController
 
   def create
     @genre_registration = Genre.new(genre_registration_params)
-     @genre_registration.save
-     redirect_to admin_genres_path
+    @genre_registration.save
+    redirect_to admin_genres_path
   end
 
   def edit
+    @genre_registration = Genre.find(params[:id])
   end
 
   def update
+    @genre_registration = Genre.find(params[:id])
+    @genre_registration.update(genre_registration_params)
+    redirect_to admin_genres_path
   end
 
   private
