b9ec949e307d8c482d404f3b9f3c910920c1c799
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-09 13:12:37 +0000

    [add] view_partial_table_part_resurrection

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,29 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @item_list = Item.page(params[:page]).per(8)
-    @item_count = Item.count
+    # @item_list = Item.page(params[:page]).per(8)
+    # @item_count = Item.count
+    # @genre_list = Genre.all
+
+    @genre_list = Genre.all
+
+    if params[:genre_id]
+      @genre = @genre_list.find(params[:genre_id])
+      all_item = @genre.items
+
+    else
+      all_item = Item.all
+
+    end
+
+    @item_list = all_item.where(is_sold: false).page(params[:page]).per(8)
+    @item_count = all_item.where(is_sold: false).count
+
   end
 
   def show
     @item = Item.find(params[:id])
+    @genre_list = Genre.all
   end
 
 end
