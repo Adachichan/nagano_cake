f783bbccae138d892e835d7aac7c7baf64ee40c9
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-17 14:05:10 +0000

    [middle] public_top_view

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,10 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
+    @genre_list = Genre.all
+    all_item = Item.all
+    @item_list = all_item.where(is_sold: false).order(updated_at: :desc).first(4)
+
   end
 
   def about
