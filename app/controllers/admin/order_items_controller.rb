40876bdb7378042a175f025206e28fa5de43a2f3
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-18 14:54:43 +0000

    [middle2] public_order_and_order_item_update_action

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -7,8 +7,8 @@ class Admin::OrderItemsController < ApplicationController
     @order = @order_item.order
     @order_item.update(product_status: params[:order_item][:product_status])
 
-    order_item_in_production_count = @order.order_items.where(product_status: 'in_production').count
-    order_item_production_complete_count = @order.order_items.where(product_status: 'production_complete').count
+    order_item_in_production_count = @order.order_items.where(product_status: OrderItem.product_statuses.key(2)).count
+    order_item_production_complete_count = @order.order_items.where(product_status: OrderItem.product_statuses.key(3)).count
     order_item_record_count = @order.order_items.count
 
     if order_item_in_production_count > 0
