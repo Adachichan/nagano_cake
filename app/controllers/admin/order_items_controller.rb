59334a65b49a76c4838faeab6358a2939ee67378
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-18 14:26:04 +0000

    [middle] public_order_and_order_item_update_action

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -3,5 +3,12 @@ class Admin::OrderItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def update
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
+
+
+    byebug
+    @order_item.update(product_status: params[:order_item][:product_status])
   end
+
 end
