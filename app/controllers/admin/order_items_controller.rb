fb2e14841ea4b8a639e0850c80cb467474616b0b
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-18 14:52:34 +0000

    [middle2] public_order_and_order_item_update_action

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -5,10 +5,22 @@ class Admin::OrderItemsController < ApplicationController
   def update
     @order_item = OrderItem.find(params[:id])
     @order = @order_item.order
-
-
-    byebug
     @order_item.update(product_status: params[:order_item][:product_status])
+
+    order_item_in_production_count = @order.order_items.where(product_status: 'in_production').count
+    order_item_production_complete_count = @order.order_items.where(product_status: 'production_complete').count
+    order_item_record_count = @order.order_items.count
+
+    if order_item_in_production_count > 0
+      @order.update(order_status: Order.order_statuses.key(2))
+    end
+
+    if order_item_production_complete_count == order_item_record_count
+      @order.update(order_status: Order.order_statuses.key(3))
+    end
+
+    redirect_to admin_order_path(@order.id)
+
   end
 
 end
