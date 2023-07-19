78db55bc18ab859eea59b8ef20f2a5196efe9b04
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-07-19 11:19:48 +0000

    [add] public_order_and_order_item_update_action

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,6 +8,20 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_item_list = @order.order_items
+    @order.update(order_status: params[:order][:order_status])
+
+    if params[:order][:order_status] == Order.order_statuses.key(1)
+
+      @order_item_list.each do |order_item|
+        order_item.update(product_status: OrderItem.product_statuses.key(1))
+      end
+
+    end
+
+    redirect_to admin_order_path(@order.id)
+
   end
 
 end
